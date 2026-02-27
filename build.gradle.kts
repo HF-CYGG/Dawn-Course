df67ba6984efcb373918b1e44ea7e02b10d431ef
YeMiao_cats <yemiao@example.com>
2026-02-27 20:47:14 +0800

    chore: 升级构建配置以提升性能和安全性
    - 增加 Gradle 的堆内存和元空间大小以改善构建性能
    - 升级 Netty 依赖版本以修复已知的安全漏洞

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ allprojects {
             force("com.google.protobuf:protobuf-java:3.25.3")
             force("org.apache.commons:commons-compress:1.26.0")
             // Fix Netty vulnerabilities
-            force("io.netty:netty-codec-http2:4.1.107.Final")
-            force("io.netty:netty-handler:4.1.107.Final")
+            force("io.netty:netty-codec-http2:4.1.108.Final")
+            force("io.netty:netty-handler:4.1.108.Final")
         }
     }
 }
