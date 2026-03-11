b81c495e7a7a72405c75e571edbf00d58108e5a1
YeMiao_cats <yemiao@example.com>
2026-03-11 21:30:37 +0800

    build: 更新应用版本号至 1.0.4.29 (versionCode 130)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.dawncourse.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 111
-        versionName = "1.0.4.10"
+        versionCode = 130
+        versionName = "1.0.4.29"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
